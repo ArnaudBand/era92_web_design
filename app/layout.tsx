fc9f88d4584882438adc92d466996d71300efd34
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-10-31 19:03:00 +0000

    Import Leaflet CSS in layout.tsx for map styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import "leaflet/dist/leaflet.css";
 import Navbar from "@/components/Navbard";
 import Footer from "@/components/Footer";
 
