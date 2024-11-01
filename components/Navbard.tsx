02c93ee9e5ee485d9d59970943beabaa639cf306
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-11-01 06:18:36 +0000

    Style Navbar component: update link styling and add border to container

--- components/Navbard.tsx
+++ components/Navbard.tsx
@@ -11,8 +11,8 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
-          <div className="flex items-center">
-            <Link className="text-black" href="/">
+          <div className="flex items-center border border-spacing-1 border-black/90 rounded-full">
+            <Link className="text-black font-bold italic" href="/">
               Watu Care
             </Link>
           </div>
