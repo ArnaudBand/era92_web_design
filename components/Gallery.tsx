c64bb55bfd5e617dee9cdf939a7d4428b1530d52
Arnaud Bandonkeye <bandonkeyea@gmail.com>
2024-11-01 11:26:08 +0000

    Update Gallery component: replace image source and clean up unused images

--- components/Gallery.tsx
+++ components/Gallery.tsx
@@ -1,4 +1,3 @@
-// components/Gallery.tsx
 import React from 'react';
 import Image from 'next/image';
 
@@ -9,21 +8,13 @@ interface GalleryProps {
 
 const Gallery: React.FC<GalleryProps> = ({ children, className }) => {
   const images = [
-    '/assets/i1.jpg',
-    '/assets/i2.jpg',
-    '/assets/i3.jpg',
-    '/assets/i4.jpg',
-    '/assets/i5.jpg',
-    '/assets/i6.jpg',
-    '/assets/i7.jpg',
-    '/assets/i8.jpg',
     '/assets/i9.jpg',
   ];
 
   return (
     <div className={`relative min-h-screen ${className}`}>
       <div className="p-4 h-1/4">
-        <Image src={images[8]} layout="fill" objectFit="cover" alt='bg-image' />
+        <Image src={images[0]} layout="fill" objectFit="cover" alt='bg-image' />
       </div>
       {/* Center children */}
       <div className="absolute inset-0 flex items-center justify-center" >
